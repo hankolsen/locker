a96fb774886d1fe4b64b98892d58a0098edd1a4f
Hank <hank@mullvad.net>
2025-09-10 15:47:49 +0200

    Clean up

---
+++ verisure.ts
@@ -0,0 +1,38 @@
+import Verisure from "verisure";
+import { overviewOperation } from "./operations";
+
+let installations: VerisureInstallation[] = [];
+let doorLockLabels: string[] = [];
+
+export const setupVerisure = async () => {
+  const verisure = new Verisure(
+    process.env.VERISURE_EMAIL ?? "",
+    process.env.VERISURE_PASSWORD ?? "",
+  );
+
+  await verisure.getToken();
+  installations = await verisure.getInstallations();
+  const overview = await installations[0]?.client(overviewOperation);
+  if (overview) {
+    doorLockLabels = overview.installation.doorlocks.map(
+      ({ device }) => device.deviceLabel,
+    );
+  }
+};
+
+export const lockDoors = async () => {
+  doorLockLabels.forEach((deviceLabel) => {
+    if (installations?.[0]) {
+      installations[0].client({
+        operationName: "DoorLock",
+        variables: {
+          deviceLabel,
+          input: { code: process.env.VERISURE_LOCK_CODE },
+        },
+        query: `mutation DoorLock($giid: String!, $deviceLabel: String!, $input: LockDoorInput!) {
+            transactionId: DoorLock(giid: $giid, deviceLabel: $deviceLabel, input: $input)
+          }`,
+      });
+    }
+  });
+};
