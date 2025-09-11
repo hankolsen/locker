06613a7624b27fb6c56548831cbeff554d59e444
Hank <hank@mullvad.net>
2025-09-11 09:20:53 +0200

    Reconnect on close

--- socket.ts
+++ socket.ts
@@ -1,17 +1,33 @@
 /* Accept self signed certificate */
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-export const setupSocketClient = () => {
-  const socket = new WebSocket(
+const RECONNECT_INTERVAL = 30_000;
+let intervalTimer: NodeJS.Timeout | undefined = undefined;
+let socket: WebSocket;
+
+const openConnection = () => {
+  socket = new WebSocket(
     `${process.env.SPC_WS_URL}?username=${process.env.SPC_WS_USER_NAME}&password=${process.env.SPC_WS_PASSWORD}`,
   );
+};
+
+export const setupSocketClient = () => {
+  openConnection();
 
   socket.addEventListener('open', () => {
     console.log('Connected to server');
+    clearInterval(intervalTimer);
+    intervalTimer = undefined;
   });
 
   socket.addEventListener('close', (event) => {
     console.log('WebSocket connection closed:', event.code, event.reason);
+    if (intervalTimer === undefined) {
+      intervalTimer = setInterval(() => {
+        console.log('Reconnecting');
+        openConnection();
+      }, RECONNECT_INTERVAL);
+    }
   });
 
   socket.addEventListener('error', (error) => {
